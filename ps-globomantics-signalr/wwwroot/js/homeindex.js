598fd6b88dd50e2237ab9aed6ebae3e5a995393f
taras-gr <tar.grytsenko@gmail.com>
2025-08-28 11:38:32 +0300

    Added initial project files

---
+++ ps-globomantics-signalr/wwwroot/js/homeindex.js
@@ -0,0 +1,11 @@
+﻿const submitBid = (auctionId) => {
+    const bid = document.getElementById(auctionId + "-input").value;
+    fetch("/auction/" + auctionId + "/newbid?currentBid=" + bid, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    location.reload();
+}
+
